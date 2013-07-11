6d133965d7ca020ceb7bb4ee4d0153676cde3ca6
Sebastien Cagnon <scagnon@aldebaran-robotics.com>
2013-07-11 05:57:59 +0000

    web.js reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  indexBuf = fs.readFileSync('index.html');
+  response.send(indexBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
