cd08f19346476780c0bc09bf66214edaed7e35a5
sebastien cagnon <cagnonsebastien@gmail.com>
2013-08-11 17:38:24 +0900

    try with createServer

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express')
   , fs      = require('fs');
 
-var app = express();
+var app = express.createServer(express.logger());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8080);
